6b13a0549b89b00509b550a615d22be5fb0902bd
Krinopotam <omegatester@gmail.com>
2025-12-05 16:49:59 +0300

    working on article

--- posts/test.ps1
+++ posts/test.ps1
@@ -1,9 +1,16 @@
-Class MyTest {
-    [string]GetVar() {
-        return "var 2"
+$array = @("A", "B", "C")
+Write-Host "Old items count: $($array.Count)"
+
+Set-StrictMode -Version Latest
+
+function Add-Item {
+    param($item)
+    
+    if ($array) {
+        Write-Host "Add item"
+        $array += $item
     }
 }
 
-$var = [MyTest]::new()
-$var.GetVar()
-[MyTest].Assembly
+Add-Item "New Item"
+Write-Host "New items count: $($array.Count)"
